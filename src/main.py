2fd8fd9fd73bf7b38e3a446f9042ca9480c33173
Anil Kumar Reddy Karna <karnaanilreddy@gmail.com>
2024-09-09 22:28:13 +0530

    conflict fixes

--- src/main.py
+++ src/main.py
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 hello
-=======
 features added for sprint-2
 hello updated master
->>>>>>> 483f412091655aad5167d7bc5b1c3b5dfca6816e
+
--- test/main.py
+++ test/main.py
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 sprint-1 addedd
-=======
 hello updated test master
->>>>>>> 483f412091655aad5167d7bc5b1c3b5dfca6816e
